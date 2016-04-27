5bca2e5f092156ad6a59ea755e461b77f577af33
jlu7 <tontonrue@gmail.com>
2016-04-26 20:19:12 -0700

    Work in progress for Buttons and title screen
    Work in progress for Buttons and title screen

---
+++ Assets/Scripts/Utility/Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour 
+{
+    public void Event()
+    {
+        return ;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+                if (hit.collider != null)
+                {
+                    Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+            }
+        }
+    }
+}
