d91868fbe17b68fbdd284f356310911ef52ad24a
Tonton Rue <tontonrue@playstudios.com>
2018-02-15 22:45:41 -0800

    Began rewrite of the dash system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,11 +4,12 @@ using System.Collections;
 
 public class PlayerController : Actor
 {
+    private const float MaxDistance = 3;
 
     public int Jumps = 2;
 
     private IEnumerator COCharacterMovement;
-    public float speed = 3f;
+    public float speed = 4f;
     public TriggerVolume triggerVolume;
 
     private bool doOnce = false;
@@ -17,7 +18,8 @@ public class PlayerController : Actor
     public enum PlayerState
     {
         Dashing,
-        NotDashing
+        NotDashing,
+        WallHug,
     }
 
     public PlayerState State = PlayerState.NotDashing;
@@ -27,9 +29,10 @@ public class PlayerController : Actor
 
     void Start()
     {
-        GetComponent<Rigidbody2D>().fixedAngle = true;
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
 		PlayerStateChanged(PlayerState.NotDashing);
         triggerVolume.OnVolumeEntered += OnCollisionDetected;
+        StartCoroutine(MovementControls());
     }
 
 	void OnDestroy()
@@ -43,51 +46,111 @@ public class PlayerController : Actor
 		if(null != PlayerStateChange) PlayerStateChange(pState);
 	}
 
-    public void FixedUpdate()
+    private IEnumerator CurrentAction = null;
+    bool WallHit = false;
+
+    public IEnumerator MovementControls()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        while (true)
         {
-            this.GetComponent<Rigidbody2D>().gravityScale = -this.GetComponent<Rigidbody2D>().gravityScale;
+            Debug.Log(GetComponent<Rigidbody2D>().gravityScale);
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                this.GetComponent<Rigidbody2D>().gravityScale = -this.GetComponent<Rigidbody2D>().gravityScale;
+            }
+
+            // End of Dash
+            if (State == PlayerState.NotDashing)
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 1;
+                CurrentAction = null;
+                WallHit = false;
+            }
+
+            // Check to see if player has clicked to dash
+            if (Input.GetMouseButtonDown(0) && Jumps > 0)
+            {
+                PlayerStateChanged(PlayerState.Dashing);
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                pos = ConstrainClickDistance(pos);
+
+                Jumps--;
+
+                GetComponent<Rigidbody2D>().gravityScale = 0;
+
+                CurrentAction = SimpleDash(new Vector3(pos.x, pos.y, this.transform.position.z));
+
+                StartCoroutine(CurrentAction);
+            }
+            yield return null;
         }
-
-        Vector3 pos = new Vector3();
-
-        // Counts Dash Frames
-        if (count > 0)
-        {
-            count--;
-        } 
-        
-        // End of Dash
-        if (doOnce && count == 0)
-        {
-            //State = PlayerState.NotDashing;
-			PlayerStateChanged(PlayerState.NotDashing);
-            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
-            GetComponent<Rigidbody2D>().gravityScale = 1;
-            doOnce = false;
-        }
-
-        // Check to see if player has clicked to dash
-        if (Input.GetMouseButtonDown(0) && !doOnce && Jumps > 0)
-        {
-			PlayerStateChanged(PlayerState.Dashing);
-            count = 5;
-            pos = Input.mousePosition;
-            pos= Camera.main.ScreenToWorldPoint(pos);
-            //Debug.Log(pos);
-            pos = pos - transform.position;
-            doOnce = true;
-            GetComponent<Rigidbody2D>().gravityScale = 0;
-            this.GetComponent<Rigidbody2D>().AddForce(pos * 500);
-
-            Jumps--;
-        }
-		
-        //Debug.Log(transform.rotation);
     }
 
-	void OnCollisionDetected(Actor collideActor)
+    private Vector3 ConstrainClickDistance(Vector3 clickTarget)
+    {
+        Vector3 finalPosition = new Vector3(0, 0, 0);
+
+        finalPosition.z = this.transform.position.z;
+
+        if ((clickTarget.x - transform.position.x) > MaxDistance)
+        {
+            finalPosition.x = transform.position.x + MaxDistance;
+        }
+        else if ((clickTarget.x - transform.position.x) < -MaxDistance)
+        {
+            finalPosition.x = transform.position.x - MaxDistance;
+        }
+        else
+        {
+            finalPosition.x = clickTarget.x;
+        }
+
+        if ((clickTarget.y - transform.position.y) > MaxDistance)
+        {
+            finalPosition.y = transform.position.y + MaxDistance;
+        }
+        else if ((clickTarget.y - transform.position.y) < -MaxDistance)
+        {
+            finalPosition.y = transform.position.y - MaxDistance;
+        }
+        else
+        {
+            finalPosition.y = clickTarget.y;
+        }
+
+        Debug.Log("Final Click Position: " + finalPosition);
+
+        return finalPosition;
+    }
+
+    private IEnumerator SimpleDash(Vector3 position)
+    {
+        while (true)
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            float step = 10 * Time.deltaTime;
+            transform.position =
+                Vector3.MoveTowards(
+                    transform.position,
+                    position, step);
+
+            if (Vector3.Distance(transform.position, position) < 0.01f)
+            {
+                break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        //FREEZE
+
+        yield return new WaitForSeconds(1f);
+
+        State = PlayerState.NotDashing;
+    }
+
+    void OnCollisionDetected(Actor collideActor)
 	{
 		//Debug.Log("OnCollisionDetected: " + reporter.GetID() + " , " + collider.GetID());
 		if(collideActor.GetID() == "Enemy")
@@ -108,6 +171,7 @@ public class PlayerController : Actor
         if (coll.gameObject.tag == "Wall")
         {
             Jumps = 2;
+            WallHit = true;
         }
 
         if (coll.gameObject.tag == "Death")
@@ -127,5 +191,4 @@ public class PlayerController : Actor
 		{
 		}
     }
-
 }
